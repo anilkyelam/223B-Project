b8fa0d8066c01cbbc413354380c03bb571e6e9b9
Audrey-Randall <audrey.randall@colorado.edu>
2019-06-01 15:59:04 -0700

    More recent version of analyze_partition.py

--- analyze_partition.py
+++ analyze_partition.py
@@ -1,24 +1,53 @@
-import multiprocessing as mp
+import matplotlib
+matplotlib.use('Agg')
 
-nodes = {}
+import numpy as np
+import matplotlib.pyplot as plt
+
 # print("Number of processors: ", mp.cpu_count())
 
 def readLAWSfile(filename):
+    nodes = []
     with open(filename) as lawsGraph:
-        for line in lawsGraph:
+        for i, line in enumerate(lawsGraph):
             line = line.rstrip()
             node = line.split()
-            nodes[int(node[0])] = len(node[1:])
+            nodes.append(len(node))
             # print(line)
-
-def testPartitioning(numWorkers):
+    return nodes
+        
+def testPartitioning(numWorkers, nodes):
     # Use round robin partitioning by hashing the vertices to see how many edges each worker would get
     workers = [0] * numWorkers
-    for key in nodes.keys():
-        workers[key%numWorkers] += nodes[key]
-    print(workers)
+    for i, numEdges in enumerate(nodes):
+        workers[i%numWorkers] += numEdges
+    mx = max(workers)
+    mn = min(workers)
+    return (mx - mn)/float(mx)*100
 
-readLAWSfile("laws_small.graph-txt")
-testPartitioning(4)
+def varyNumPartitions(nodes, lo=3, hi=1000, step=1):
+    percentDiffs = []
+    for i in np.arange(lo, hi, step):
+        percentDiffs.append(testPartitioning(i, nodes))
+    return percentDiffs
 
+def plotDiffsVsPartitions(lo, hi, step=1):
+    files = ["enwiki-2015.graph-txt", "twitter.graph-txt", "uk-2007-05.graph-txt", "wordassociation-2011.graph-txt"]
+    for f in files:
+        filename = "inputs/" + f
+        print("Reading ", f, "...")
+        nodes = readLAWSfile(filename)
+        print("Plotting ", f, "...")
+        percentDiffs = varyNumPartitions(nodes, lo, hi, step)
+        plt.plot(np.arange(lo, hi, step), percentDiffs, label=f.split('.')[0])
+
+    plt.xlabel("Partitions")
+    plt.ylabel("Range of number of edges/node (%)")
+    plt.title("Range of edges/node vs. number of partitions\n for various graphs")
+    plt.legend()
+    plt.savefig("test_small.png")
+    plt.show()
+
+# readLAWSfile("inputs/enwiki-2015.graph-txt")
+plotDiffsVsPartitions(3, 100, 20)
 # print(nodes)
